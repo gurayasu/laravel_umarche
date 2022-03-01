a82a73b573486fe25d48fd9aa7b16e318beee9c8
gurayasu <yasu.23721740311@gmail.com>
2022-03-01 23:09:45 +0900

    Store

--- app/Http/Controllers/Admin/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Admin/Auth/RegisteredUserController.php
@@ -49,6 +49,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(RouteServiceProvider::ADMIN_HOME);
+        return redirect(RouteServiceProvider::HOME);
     }
 }
